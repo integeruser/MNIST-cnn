db0fd032fce15a30fa81e342315ba53407b89392
Francesco Cagnin <francesco.cagnin@gmail.com>
2015-09-02 17:19:06 +0200

    changed all ' to " for consistency minor formatting

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
-__author__ = 'F. Cagnin and A. Torcinovich'
+__author__ = "F. Cagnin and A. Torcinovich"
+
+import NeuralNetwork
 
 import mnist_loader
-import NeuralNetwork
 import numpy as np
 
 
 np.random.seed(314)
+
 print("Loading data")
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
 print("Generating desired CNN")
-# vec = [784, ('cl', 5, 1, 'sigmoid'), ('pl', 2, 'mean'), ('fcl', 100, 'sigmoid'), ('fcl', 10, 'sigmoid')]
-vec = [784,('fcl',100,'sigmoid'),('fcl',10,'sigmoid')]
-net = NeuralNetwork.ConvolutionalNeuralNetwork(vec, 'quadratic')
+# vec = [784, ("cl", 5, 1, "sigmoid"), ("pl", 2, "mean"), ("fcl", 100, "sigmoid"), ("fcl", 10, "sigmoid")]
+vec = [784, ("fcl", 100, "sigmoid"), ("fcl", 10, "sigmoid")]
+net = NeuralNetwork.ConvolutionalNeuralNetwork(vec, "quadratic")
+
 print("Training CNN")
 net.training(list(training_data), 30, 10, 3.0)
+
 print("Testing CNN performances")
 net.testing(list(test_data))
