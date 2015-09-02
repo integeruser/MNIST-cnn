1512f8a4ec319f730b40b6691dc6dc5bfe27c8a7
Francesco Cagnin <francesco.cagnin@gmail.com>
2015-09-02 17:25:13 +0200

    fixed ConvolutionalNeuralNetwork into NeuralNetwork

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 print("Generating desired CNN")
 # vec = [784, ("cl", 5, 1, "sigmoid"), ("pl", 2, "mean"), ("fcl", 100, "sigmoid"), ("fcl", 10, "sigmoid")]
 vec = [784, ("fcl", 100, "sigmoid"), ("fcl", 10, "sigmoid")]
-net = NeuralNetwork.ConvolutionalNeuralNetwork(vec, "quadratic")
+net = NeuralNetwork.NeuralNetwork(vec, "quadratic")
 
 print("Training CNN")
 net.training(list(training_data), 30, 10, 3.0)
