3726eaca2b5de99ec1f462ec2ba801fd1a93ccd3
Francesco Cagnin <francesco.cagnin@gmail.com>
2015-12-02 12:00:26 +0100

    [updated] to use the MNIST dataset built using the functions in 'utils.py'

--- mnist_fcl.py
+++ mnist_fcl.py
@@ -1,51 +1,32 @@
 #!/usr/bin/env python3
-import argparse
+import sys
 
-from layers import *
-import network
-
-
-def load_mnist_dataset(dataset_path):
-    import gzip
-    import pickle
-
-    def to_categorical(y):
-        y = np.asarray(y, dtype="int32")
-        nb_classes = np.max(y) + 1
-        Y = np.zeros((len(y), nb_classes))
-        for i in range(len(y)):
-            Y[i, y[i]] = 1.
-        return Y
-
-    with gzip.open(dataset_path, "rb") as f:
-        tr_d, va_d, te_d = pickle.load(f, encoding="latin1")
-        training_inputs = [np.reshape(x, (x.size, 1)) for x in tr_d[0]]
-        training_data = zip(training_inputs, (np.reshape(y, (y.size, 1)) for y in to_categorical(tr_d[1])))
-        validation_inputs = [np.reshape(x, (x.size, 1)) for x in va_d[0]]
-        validation_data = zip(validation_inputs, va_d[1])
-        test_inputs = [np.reshape(x, (x.size, 1)) for x in te_d[0]]
-        test_data = zip(test_inputs, (np.reshape(y, (y.size, 1)) for y in to_categorical(te_d[1])))
-        return training_data, validation_data, test_data
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("dataset_path")
-args = parser.parse_args()
+import functions as f
+import layers as l
+import network as n
+import numpy as np
+import utils as u
 
 np.random.seed(314)
 
-print("Loading data")
-training_data, validation_data, test_data = load_mnist_dataset(args.dataset_path)
+if len(sys.argv) != 2:
+    print("Usage: %s mnist_npzpath")
+    sys.exit(2)
 
-print("Generating desired CNN")
-net = network.NeuralNetwork([
-    InputLayer(height=28, width=28),
-    FullyConnectedLayer(height=100, width=1, act_func=functions.sigmoid),
-    FullyConnectedLayer(height=10, width=1, act_func=functions.sigmoid)
-], functions.quadratic)
+mnist_npzpath = sys.argv[1]
 
-print("Training CNN")
-network.train(net, list(training_data), 30, 10, 3.0)
+print("Loading '%s'..." % mnist_npzpath)
+trn_set, tst_set = u.load_mnist_npz(mnist_npzpath)
 
-print("Testing CNN performances")
-network.test(net, list(test_data))
+print("Building NN...")
+net = n.NeuralNetwork([
+    l.InputLayer(height=28, width=28),
+    l.FullyConnectedLayer(height=100, width=1, act_func=f.sigmoid),
+    l.FullyConnectedLayer(height=10, width=1, act_func=f.sigmoid)
+], f.quadratic)
+
+print("Training NN...")
+n.train(net, trn_set, 1, 10, 3.0)
+
+print("Testing NN...")
+n.test(net, tst_set)
