237576825a8eaf1c396c6953f2a6df8b2a60ffdf
Francesco Cagnin <francesco.cagnin@gmail.com>
2015-12-05 11:10:06 +0100

    cleanup

--- utils.py
+++ utils.py
@@ -59,13 +59,13 @@ def glorot_uniform(size, num_neurons_in, num_neurons_out):
 
 
 class bcolors:
-    HEADER =    "\033[95m"
-    OKBLUE =    "\033[94m"
-    OKGREEN =   "\033[92m"
-    WARNING =   "\033[93m"
-    FAIL =      "\033[91m"
-    ENDC =      "\033[0m"
-    BOLD =      "\033[1m"
+    HEADER    = "\033[95m"
+    OKBLUE    = "\033[94m"
+    OKGREEN   = "\033[92m"
+    WARNING   = "\033[93m"
+    FAIL      = "\033[91m"
+    ENDC      = "\033[0m"
+    BOLD      = "\033[1m"
     UNDERLINE = "\033[4m"
 
 def print(s="", bcolor=None, override=False):
