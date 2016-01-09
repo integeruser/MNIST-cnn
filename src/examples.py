a56d77d8940ceb76347485ccfc786378018d7d5f
Francesco Cagnin <francesco.cagnin@gmail.com>
2016-01-10 00:32:09 +0100

    added few more examples

--- src/examples.py
+++ src/examples.py
@@ -11,9 +11,43 @@ import network as n
 import utils as u
 
 
-def fcl01():  # 91.75%
+def fcl01():  # 90.21%
     net = n.NeuralNetwork([
         l.InputLayer(height=28, width=28),
+        l.FullyConnectedLayer(10, act_func=f.sigmoid)
+    ], f.quadratic)
+    optimizer = {"type": "SGD", "eta": 0.1}
+    num_epochs = 1
+    batch_size = 10
+    return net, optimizer, num_epochs, batch_size
+
+def fcl02():  # 95.72%
+    net = n.NeuralNetwork([
+        l.InputLayer(height=28, width=28),
+        l.FullyConnectedLayer(100, act_func=f.sigmoid),
+        l.FullyConnectedLayer(10, act_func=f.sigmoid)
+    ], f.quadratic)
+    optimizer = {"type": "SGD", "eta": 3.0}
+    num_epochs = 1
+    batch_size = 10
+    return net, optimizer, num_epochs, batch_size
+
+def fcl03():  # 91.75%
+    net = n.NeuralNetwork([
+        l.InputLayer(height=28, width=28),
+        l.FullyConnectedLayer(10, act_func=f.softmax)
+    ], f.log_likelihood)
+    optimizer = {"type": "SGD", "eta": 0.1}
+    num_epochs = 1
+    batch_size = 10
+    return net, optimizer, num_epochs, batch_size
+
+
+def cnn01():  # 88.13%
+    net = n.NeuralNetwork([
+        l.InputLayer(height=28, width=28),
+        l.ConvolutionalLayer(2, kernel_size=5, act_func=f.sigmoid),
+        l.MaxPoolingLayer(pool_size=2),
         l.FullyConnectedLayer(height=10, act_func=f.softmax)
     ], f.log_likelihood)
     optimizer = {"type": "SGD", "eta": 0.1}
@@ -21,13 +55,13 @@ def fcl01():  # 91.75%
     batch_size = 10
     return net, optimizer, num_epochs, batch_size
 
-def cnn01():  # 88.13%
+def cnn02():  # 84.60%
     net = n.NeuralNetwork([
         l.InputLayer(height=28, width=28),
-        l.ConvolutionalLayer(depth=2, kernel_size=5, act_func=f.sigmoid),
+        l.ConvolutionalLayer(2, kernel_size=5, act_func=f.sigmoid),
         l.MaxPoolingLayer(pool_size=2),
         l.FullyConnectedLayer(height=10, act_func=f.softmax)
-    ], f.log_likelihood)
+    ], f.categorical_crossentropy)
     optimizer = {"type": "SGD", "eta": 0.1}
     num_epochs = 1
     batch_size = 10
